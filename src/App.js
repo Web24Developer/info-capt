60b860fed483501a0e7e096295fd0d37662a7079
Web24Developer <sreeelakshmisunil@gmail.com>
2024-04-26 16:41:47 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleSubmit=()=>{
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App'>
+        <div className='form-container'>
+          <div>
+            <h2>InfoCapture</h2>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <input type="text" name='firstname' placeholder='Firstname' required/>
+            <input type="text" name='lastname' placeholder='lastname' required/>
+            <input type="text" name='city' placeholder='city' required/>
+            <button className='addBtn'>Add Record</button>
+            <button className='updateBtn'>Update Record</button>
+          </form>
+        </div>
+        <div className='table-container'>
+            <h2>DigiPunk World Citizens</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Firstname</th>
+                  <th>Lastname</th>
+                  <th>City</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Sreelakshmi</td>
+                  <td>Sunil</td>
+                  <td>Wayanad</td>
+                  <td>
+                  <i class="fa-solid fa-pen-to-square editBtn myBtn"></i>
+                  <i class="fa-solid fa-rectangle-xmark delBtn myBtn"></i>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+        </div>
+        <div className='footer'>
+        ©{new Date().getFullYear()}InfoCapture.All rights reserved.
+        </div>
+      </div>
+    </>
   );
 }
 
